fff1447e8d4f1dfeda0351d46aac667d099dd4b7
schperplata <domen.jurkovic@gmail.com>
2022-11-12 19:50:52 +0100

    prepare venv script moved to scripts dir

--- scripts/prepare_venv.bat
+++ scripts/prepare_venv.bat
@@ -4,11 +4,13 @@ SET PY_EXE_PATH="C:\Python311\python.exe"
 
 SET VENV_NAME=venv_py311
 
+cd ..
+
 %PY_EXE_PATH% -m venv %VENV_NAME%
 
 CALL "%VENV_NAME%/Scripts/activate.bat"
 
 python -m pip install -U pip
-python -m pip install -r requirements_dev.txt
+python -m pip install -U -r requirements_dev.txt
 python -m pip install -e .
 pause
