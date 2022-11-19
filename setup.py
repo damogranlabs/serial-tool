1ad699687d2b651c9a714b2da14a1c418fa483e5
schperplata <domen.jurkovic@gmail.com>
2022-11-19 12:18:36 +0100

    Updated classifiers, added app entry point

--- setup.py
+++ setup.py
@@ -26,17 +26,29 @@ setup(
     long_description=read_file("README.md"),
     keywords=["serial tool", "UART", "serial", "RS232"],
     version=get_version("src/serial_tool/__init__.py"),
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Communications",
+        "Topic :: Terminals :: Serial",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
+        "Programming Language :: Python :: 3.9"
+        "Programming Language :: Python :: 3.10"
+        "Programming Language :: Python :: 3.11",
+        "Environment :: Console",
     ],
     license="MIT license",
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=["PyQt5", "aioserial", "pyserial"],
     include_package_data=False,
+    entry_points={
+        "console_scripts": ["serial-tool=serial_tool.app:main"],
+    },
 )
